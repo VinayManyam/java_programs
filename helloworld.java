443285f60ad1c6a82748bfb405d3eaeb51800242
vinay manyam <vinayip541@gmail.com>
2017-08-18 16:53:46 -0700

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+class demo
+{
+static public void main(String args[]){
+System.out.print("hello world");
+}
+
+}
+
+class demo2
+{
+static public void main(String args[]){
+System.out.print("hello world2");
+}
+
+}
