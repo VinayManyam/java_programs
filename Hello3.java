b0dc772cfdc37b14b2db1336097ebf5d4ce883b8
vinay manyam <vinayip541@gmail.com>
2017-09-09 10:48:52 +0530

    Add files via upload

---
+++ Hello3.java
@@ -0,0 +1,18 @@
+//import java.lang.*;
+public class Hello3{
+	public static void main(String[] a){
+		System.out.print("hello java");
+	}
+	
+}
+
+class Hello4{
+	public static void main(String[] a){
+		System.out.print("hello java4");
+	}
+
+}
+
+
+
+
---
+++ hello2.java
@@ -0,0 +1,17 @@
+//import java.lang.*;
+public class hello2{
+	public static void main(String[] a){
+		System.out.print("hello java2");
+	}
+}
+	/*
+hello2.java:2: error: class hello3 is public, should be declared in a file named hello3.java
+public class hello3{
+       ^
+1 error}
+*/
+
+
+
+
+
