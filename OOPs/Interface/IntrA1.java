1361acb90398db4a8fb993c00bc5ebe1a6f212bc
vinay manyam <vinayip541@gmail.com>
2017-10-17 09:16:36 +0530

    Create IntrA1.java

---
+++ OOPs/Interface/IntrA1.java
@@ -0,0 +1,10 @@
+package Interface;
+
+interface IntrA1 {
+double pi=3.14;
+//int x; =>The blank final field x may not have been initialized
+void m1();
+void m2();
+
+//void m3() {} => Abstract methods do not specify a body
+}
